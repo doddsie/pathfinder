b6dc4b8afa81ac54f20a331cb0696b157deb0cdc
David Dodd <ddodd@Apples-MacBook-Pro.local>
2022-05-08 11:13:12 -0700

    added car class

--- autofinder.ino
+++ autofinder.ino
@@ -1,120 +1,110 @@
 #include <Servo.h>
 
-
-const int ENA = 10;
-const int IN1 = 9;
-const int IN2 = 8;
-const int IN4 = 7;
-const int IN3 = 6;
-const int ENB = 5;
-const int ledPin = 13;
-
-
-
-const int trigPin = 3;
 const int echoPin = 2;
-float microsecondsToFeet(float microseconds)
-{
-  return microseconds / 979 / 2;
-}
+const int trigPin = 3;
+const int ENB = 5;
+const int IN3 = 6;
+const int IN4 = 7;
+const int IN2 = 8;
+const int IN1 = 9;
+const int ENA = 10;
+const int ledPin = 13;
 
 long microsecondsToInches(long microseconds)
 {
   return microseconds / 74 / 2;
 }
 
-Servo myservo;  // create servo object to control a servo
-
-
-void setup()
+class DistanceServo: public Servo 
 {
+public:
+   void setpos(int pos);
+   long  get_distance();
+};
 
-    Serial.begin(9600);
-
-  pinMode(ENA,OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  pinMode(ledPin,OUTPUT);
-  digitalWrite(ledPin, LOW);
-
-  //delay is used to control the speed, the lower the faster.
-  //reverse(step,delay);
-  //reverse(80,20);
-  //forward(step,delay);
-  //forward(80,20);
-    myservo.attach(4);  // attaches the servo on pin 9 to the servo object
-
-
-
+void DistanceServo::setpos(int pos) {
+  write(pos);              // tell servo to go to position in variable 'pos'
+  delay(15);  
 }
 
-static int i= 0;
-static bool move = true;
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
-int pos = 0;    // variable to store the servo position
-
-void loop()
-{
-  digitalWrite(ENA, HIGH);
-  digitalWrite(ENB, HIGH);
-
-  if (i++ %60== 0) {
-    move = !move;
-  }
-if (move) {  
-  Serial.println("loop 1");
-
-  // IN1 = LEFT  BACKWARD
-  // IN1 = LEFT FORWARD
-  // IN3 - RIGHT FORWARD
-  // IN4 = RIGHT backward
-
-    digitalWrite(IN1, 0);
-    digitalWrite(IN2, 0);
-    digitalWrite(IN3, 0);
-    digitalWrite(IN4, 100);
-} else {
-  Serial.println("loop 0");
-
-    digitalWrite(IN1, 0);
-    digitalWrite(IN3, 0);
-    digitalWrite(IN2, 0);
-    digitalWrite(IN4, 0);
-}
-
-    
-  long duration, inches;
-  float feet;
- pinMode(trigPin, OUTPUT);
+long DistanceServo::get_distance() {
+  pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  return microsecondsToInches(pulseIn(echoPin, HIGH));
+}
 
-  feet = microsecondsToFeet(duration);
-  inches = microsecondsToInches(duration);
-  Serial.print(feet);
-  Serial.print("FT, ");
-  Serial.print(inches);
-  Serial.print("IN, ");
-  Serial.println();
+class Car {
+    // IN1 = LEFT  BACKWARD
+    // IN2 = LEFT FORWARD
+    // IN3 - RIGHT FORWARD
+    // IN4 = RIGHT backward
+public:    
+  void forward() {
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 1);
+    digitalWrite(IN3, 1);
+    digitalWrite(IN4, 0);
+  }
    
- for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  void backward() {
+    digitalWrite(IN1, 1);
+    digitalWrite(IN2, 0);
+    digitalWrite(IN3, 0);
+    digitalWrite(IN4, 1);
   }
 
+  void left() {
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 1);
+    digitalWrite(IN3, 0);
+    digitalWrite(IN4, 1);
+  }
 
+  void right() {
+    digitalWrite(IN1, 1);
+    digitalWrite(IN2, 0);
+    digitalWrite(IN3, 1);
+    digitalWrite(IN4, 0);
+  }
+};
+
+
+DistanceServo servo;  // create servo object to control a servo
+Car           car;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+
+  servo.attach(4);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop()
+{
+  long distance = 0; 
+  servo.setpos(90);
+  distance = servo.get_distance();
+
+  if (distance < 3) {
+    car.right();
+  } else {
+    car.forward();
+  } 
+  delay(500);
 }
